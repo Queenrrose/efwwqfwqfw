665b01fd9898aaa8703cff385884e522dbffdd32
Queen Rose <00queen.rose@gmail.com>
2025-01-02 14:19:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+  TOKEN: process.env.TOKEN,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  DATABASE: process.env.DATABASE,
+  CLIENT_ID: "991639726094286918",
+  API_URL: "https://Save-Members.abdlerhman-sondos.repl.co",
+  GUILD_ID: "920341701191671938",
+  ROLE_ID: "990705427601850438",
+  PREFIX: "-",
+  DEVS: ["683799239717027888"]
+}
